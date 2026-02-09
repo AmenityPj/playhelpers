27fe8f2eb13564ba11c61a6ed2557a084c99011c
imPratik <impratikjaiswal@gmail.com>
2026-02-09 20:24:40 +0530

    6.0.2.dev4

--- play_helpers/_version.py
+++ play_helpers/_version.py
@@ -7,5 +7,5 @@ Provides play_helpers version information.
 
 from incremental import Version
 
-__version__ = Version("play_helpers", 6, 0, 2, dev=3)
+__version__ = Version("play_helpers", 6, 0, 2, dev=4)
 __all__ = ["__version__"]
